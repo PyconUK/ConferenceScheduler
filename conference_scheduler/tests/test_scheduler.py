b0cce5aa08b43c40b3cba24dfbb57e6cf762b563
Owen Campbell <owen.campbell@tanti.org.uk>
2017-04-27 14:56:16 +0100

    Add some test fixtures

--- conference_scheduler/tests/test_scheduler.py
+++ conference_scheduler/tests/test_scheduler.py
@@ -1,5 +1,22 @@
 import pulp
+from conference_scheduler.resources import Person, Room, Slot
 from conference_scheduler import scheduler
+
+people = (
+    Person(name='Alice'),
+    Person(name='Bob'),
+    Person(name='Charlie')
+)
+
+rooms = (
+    Room(name='Main Hall', capacity=500),
+    Room(name='Room 1', capacity=50)
+)
+
+slots = (
+    Slot(starts_at='15-Sep-2016 09:30', ends_at='15-Sep-2016 10:00'),
+    Slot(starts_at='15-Sep-2016 10:00', ends_at='15-Sep-2016 10:30')
+)
 
 
 def test_is_valid_schedule():
