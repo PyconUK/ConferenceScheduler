d74efb5f6759ea0f8ce95bda311e277a489a67f0
Owen Campbell <owen.campbell@tanti.org.uk>
2017-04-27 13:25:35 +0100

    Add basic file and folder structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ConferenceScheduler',
+    version='',
+    packages=[
+        'conference_scheduler',
+    ],
+    url='',
+    license='MIT',
+    author='Owen Campbell,
+    author_email='Owen Campbell <owen.campbell@tanti.org.uk>',
+    description='',
+    install_requires=['pulp'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
