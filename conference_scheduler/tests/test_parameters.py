b5c987597d3fc8c096cf570f7cd4d51055c27cfb
Owen Campbell <owen.campbell@tanti.org.uk>
2017-04-28 14:38:06 +0100

    Add params tests

--- conference_scheduler/tests/test_parameters.py
+++ conference_scheduler/tests/test_parameters.py
@@ -4,3 +4,31 @@ from conference_scheduler import parameters
 def test_variables(events, rooms, slots):
     variables = parameters.variables(events, rooms, slots)
     assert len(variables) == 24
+
+
+def test_max_one_event_per_room_per_slot(events, rooms, slots):
+    variables = parameters.variables(events, rooms, slots)
+    event_count = len(events)
+    room_count = len(rooms)
+    slot_count = len(slots)
+    constraints = parameters._max_one_event_per_room_per_slot(
+        variables, event_count, room_count, slot_count
+    )
+    assert len(constraints) == 8
+
+
+def test_only_once_per_event(events, rooms, slots):
+    variables = parameters.variables(events, rooms, slots)
+    event_count = len(events)
+    room_count = len(rooms)
+    slot_count = len(slots)
+    constraints = parameters._only_once_per_event(
+        variables, event_count, room_count, slot_count
+    )
+    assert len(constraints) == 3
+
+
+def test_constraints(events, rooms, slots):
+    variables = parameters.variables(events, rooms, slots)
+    constraints = parameters.constraints(variables, events, rooms, slots)
+    assert len(constraints) == 11
