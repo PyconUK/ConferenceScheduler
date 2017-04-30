7c91ab06978cc7d0023485bf630763e0c72c1795
Owen Campbell <owen.campbell@tanti.org.uk>
2017-04-30 18:30:14 +0100

    Rename dimensions as shape

--- conference_scheduler/parameters.py
+++ conference_scheduler/parameters.py
@@ -37,27 +37,27 @@ def variables(events: Sequence, rooms: Sequence, slots: Sequence):
     return variables
 
 
-def _max_one_event_per_room_per_slot(variables, dimensions):
+def _max_one_event_per_room_per_slot(variables, shape):
     # A room may only have a maximum of one event scheduled in any time slot
     return [
         sum(
             variables[(event_idx, room_idx, slot_idx)]
-            for event_idx in range(dimensions.events)
+            for event_idx in range(shape.events)
         ) <= 1
-        for room_idx in range(dimensions.rooms)
-        for slot_idx in range(dimensions.slots)
+        for room_idx in range(shape.rooms)
+        for slot_idx in range(shape.slots)
     ]
 
 
-def _only_once_per_event(variables, dimensions):
+def _only_once_per_event(variables, shape):
     # An event may only be scheduled in one combination of room and time slot
     return [
         sum(
             variables[(event_idx, room_idx, slot_idx)]
-            for room_idx in range(dimensions.rooms)
-            for slot_idx in range(dimensions.slots)
+            for room_idx in range(shape.rooms)
+            for slot_idx in range(shape.slots)
         ) == 1
-        for event_idx in range(dimensions.events)
+        for event_idx in range(shape.events)
     ]
 
 
@@ -65,13 +65,13 @@ def _is_suitable_room(event, room):
     return event.event_type in room.suitability
 
 
-def _room_suitability(variables, dimensions, events, rooms):
+def _room_suitability(variables, shape, events, rooms):
     # A room may only be scheduled to host an event for which it is deemed
     # suitable
     return [
         sum(
             variables[(events.index(event), rooms.index(room), slot_idx)]
-            for slot_idx in range(dimensions.slots)
+            for slot_idx in range(shape.slots)
         ) == 0
         for room in rooms for event in events
         if not _is_suitable_room(event, room)
@@ -79,10 +79,10 @@ def _room_suitability(variables, dimensions, events, rooms):
 
 
 def constraints(variables, events, rooms, slots):
-    dimensions = Shape(len(events), len(rooms), len(slots))
+    shape = Shape(len(events), len(rooms), len(slots))
     constraints = []
-    constraints.extend(_max_one_event_per_room_per_slot(variables, dimensions))
-    constraints.extend(_only_once_per_event(variables, dimensions))
-    constraints.extend(_room_suitability(variables, dimensions, events, rooms))
+    constraints.extend(_max_one_event_per_room_per_slot(variables, shape))
+    constraints.extend(_only_once_per_event(variables, shape))
+    constraints.extend(_room_suitability(variables, shape, events, rooms))
 
     return constraints
--- conference_scheduler/tests/test_parameters.py
+++ conference_scheduler/tests/test_parameters.py
@@ -7,18 +7,18 @@ def test_variables(events, rooms, slots):
 
 
 def test_max_one_event_per_room_per_slot(variables, events, rooms, slots):
-    dimensions = parameters.Shape(len(events), len(rooms), len(slots))
+    shape = parameters.Shape(len(events), len(rooms), len(slots))
     constraints = parameters._max_one_event_per_room_per_slot(
-        variables, dimensions
+        variables, shape
     )
     # There should be one constraint for each combination of room and slot
     assert len(constraints) == 8
 
 
 def test_only_once_per_event(variables, events, rooms, slots):
-    dimensions = parameters.Shape(len(events), len(rooms), len(slots))
+    shape = parameters.Shape(len(events), len(rooms), len(slots))
     constraints = parameters._only_once_per_event(
-        variables, dimensions
+        variables, shape
     )
     # There should be one constraint per event
     assert len(constraints) == 3
@@ -30,9 +30,9 @@ def test_is_suitable_room(events, rooms):
 
 
 def test_room_suitability(variables, events, rooms, slots):
-    dimensions = parameters.Shape(len(events), len(rooms), len(slots))
+    shape = parameters.Shape(len(events), len(rooms), len(slots))
     constraints = parameters._room_suitability(
-        variables, dimensions, events, rooms
+        variables, shape, events, rooms
     )
     assert len(constraints) == 3
 
