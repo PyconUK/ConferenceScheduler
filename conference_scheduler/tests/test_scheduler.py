e209388d2f515b0a2c5ed5094b8b66c90400ef3b
Owen Campbell <owen.campbell@tanti.org.uk>
2017-05-02 11:55:10 +0100

    [#1] Add some comments to the test

--- conference_scheduler/tests/test_scheduler.py
+++ conference_scheduler/tests/test_scheduler.py
@@ -3,23 +3,27 @@ from collections import Counter
 from conference_scheduler import scheduler
 
 
-def test_is_valid_schedule():
+def test_is_valid_ssolution():
     talks, slots = 2, 3
-    X = np.array(
-        [[1, 0, 0],
-        [0, 1, 0]]
-    )
+
+    # a valid solution with one talk per slot
+    X = np.array([
+        [1, 0, 0],
+        [0, 1, 0]
+    ])
+    # a list of constraints specifying that a slot may have at most one talk
     constraints = (
         sum(X[(talk, slot)] for talk in range(talks)) <= 1
         for slot in range(slots)
     )
     assert scheduler.is_valid_solution(X, constraints)
 
-
-    X = np.array(
-        [[1, 0, 0],
-        [1, 0, 0]]
-    )
+    # An invalid solution with 2 talks scheduled in slot 1
+    X = np.array([
+        [1, 0, 0],
+        [1, 0, 0]
+    ])
+    # a list of constraints specifying that a slot may have at most one talk
     constraints = (
         sum(X[(talk, slot)] for talk in range(talks)) <= 1
         for slot in range(slots)
