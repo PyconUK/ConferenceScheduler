55e504afc538fb634fb4ecdd354c13c2f1399019
Vince Knight <vincent.knight@gmail.com>
2017-05-04 19:57:16 +0100

    Add an obj function that counts number of changes

--- conference_scheduler/tests/lp_problem/test_objective_functions.py
+++ conference_scheduler/tests/lp_problem/test_objective_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from conference_scheduler.lp_problem import objective_functions as of
+from conference_scheduler.scheduler import array_to_schedule
 
 
 def test_capacity_demand_difference(slots, events, X):
@@ -23,3 +24,29 @@ def test_capacity_demand_difference_with_examples(slots, events):
     ])
     overflow = of.capacity_demand_difference(slots, events, X)
     assert overflow == -440
+
+
+def test_number_of_changes(slots, events):
+    X_orig = np.array([
+        [1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0]
+    ])
+    schedule = list(array_to_schedule(array=X_orig, slots=slots,
+                                      events=events))
+
+    assert of.number_of_changes(slots, events, schedule, X_orig) == 0
+
+    X_new = np.array([
+        [0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0]
+    ])
+    assert of.number_of_changes(slots, events, schedule, X_new) == 2
+
+    X_new = np.array([
+        [0, 1, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0]
+    ])
+    assert of.number_of_changes(slots, events, schedule, X_new) == 7
