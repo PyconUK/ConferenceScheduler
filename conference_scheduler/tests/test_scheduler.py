68c578ee709ab5c11ccb311a4b38176b333b9419
Owen Campbell <owen.campbell@tanti.org.uk>
2017-05-04 11:40:08 +0100

    [#20] Move testing of main output functions to top of module

--- conference_scheduler/tests/test_scheduler.py
+++ conference_scheduler/tests/test_scheduler.py
@@ -5,6 +5,71 @@ from conference_scheduler.resources import ScheduledItem
 from conference_scheduler.lp_problem import objective_functions as of
 
 
+# Testing of the three output functions called by external programs
+
+# Solution form
+# There is most testing here since the scheduler.solution function is the one
+# that sets up the pulp problem and returns the solution from pulp
+
+
+def test_solution_has_content(solution):
+    assert len(solution) > 0
+
+
+def test_all_events_scheduled(shape, solution):
+    scheduled_events = [item[0] for item in solution]
+    for event in range(shape.events):
+        assert event in scheduled_events
+
+
+def test_slots_scheduled_once_only(solution):
+    for slot, count in Counter(item[1] for item in solution).items():
+        assert count <= 1
+
+
+def test_events_scheduled_once_only(solution):
+    for event, count in Counter(item[0] for item in solution).items():
+        assert count == 1
+
+
+def test_optimal_schedule(slots, events, sessions):
+    solution = scheduler.solution(
+        events=events, slots=slots, sessions=sessions,
+        objective_function=of.capacity_demand_difference)
+    assert list(solution) == [(0, 3), (1, 4), (2, 0)]
+
+
+# Array form
+# Less testing needed here since it simply calls scheduler.solution and
+# converts the result to array form
+
+
+def test_array_has_content(array):
+    assert len(array) > 0
+
+
+def test_array_shape(array):
+    assert array.shape == (3, 7)
+
+
+def test_array_nonzero(array):
+    nonzero = np.transpose(np.nonzero(array))
+    assert len(nonzero) == 3
+
+# Schedule form
+# Similar to array form, there is less testsing here since it simply converts
+# the output of scheduler.solution to schedule form
+
+
+def test_schedule_has_content(schedule):
+    assert len(schedule) > 0
+
+
+def test_schedule_has_all_events(schedule, events):
+    scheduled_events = [item.event for item in schedule]
+    assert scheduled_events == list(events)
+
+
 def test_valid_array_has_no_violations(
     valid_array, events, slots, sessions
 ):
@@ -127,67 +192,3 @@ def test_schedule_unscheduled_event_has_violations(events, slots, sessions):
     assert violations == [
         'Event either not scheduled or scheduled multiple times - event: 1'
     ]
-
-# Testing of the three output functions called by external programs
-
-# Solution form
-# There is most testing here since the scheduler.solution function is the one
-# that sets up the pulp problem and returns the solution from pulp
-
-
-def test_solution_has_content(solution):
-    assert len(solution) > 0
-
-
-def test_all_events_scheduled(shape, solution):
-    scheduled_events = [item[0] for item in solution]
-    for event in range(shape.events):
-        assert event in scheduled_events
-
-
-def test_slots_scheduled_once_only(solution):
-    for slot, count in Counter(item[1] for item in solution).items():
-        assert count <= 1
-
-
-def test_events_scheduled_once_only(solution):
-    for event, count in Counter(item[0] for item in solution).items():
-        assert count == 1
-
-
-def test_optimal_schedule(slots, events, sessions):
-    solution = scheduler.solution(
-        events=events, slots=slots, sessions=sessions,
-        objective_function=of.capacity_demand_difference)
-    assert list(solution) == [(0, 3), (1, 4), (2, 0)]
-
-
-# Array form
-# Less testing needed here since it simply calls scheduler.solution and
-# converts the result to array form
-
-
-def test_array_has_content(array):
-    assert len(array) > 0
-
-
-def test_array_shape(array):
-    assert array.shape == (3, 7)
-
-
-def test_array_nonzero(array):
-    nonzero = np.transpose(np.nonzero(array))
-    assert len(nonzero) == 3
-
-# Schedule form
-# Similar to array form, there is less testsing here since it simply converts
-# the output of scheduler.solution to schedule form
-
-
-def test_schedule_has_content(schedule):
-    assert len(schedule) > 0
-
-
-def test_schedule_has_all_events(schedule, events):
-    scheduled_events = [item.event for item in schedule]
-    assert scheduled_events == list(events)
