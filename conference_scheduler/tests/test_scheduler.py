e5cdbb6d8b93eda76723cc9d57ccbbbec8a5a96a
Owen Campbell <owen.campbell@tanti.org.uk>
2017-05-04 13:56:45 +0100

    [#23] Add test for solution_to_array function

--- conference_scheduler/tests/test_scheduler.py
+++ conference_scheduler/tests/test_scheduler.py
@@ -72,10 +72,14 @@ def test_schedule_has_all_events(schedule, events):
 
 # Testing the conversion between various forms of schedule output
 
+def test_solution_to_array(valid_solution, valid_array, events, slots):
+    array = scheduler.solution_to_array(valid_solution, events, slots)
+    assert np.array_equal(array, valid_array)
+
 
 def test_schedule_to_array(valid_schedule, valid_array, events, slots):
     array = scheduler.schedule_to_array(valid_schedule, events, slots)
-    assert np.array_equal(array, array)
+    assert np.array_equal(array, valid_array)
 
 
 def test_array_to_schedule(valid_schedule, valid_array, events, slots):
