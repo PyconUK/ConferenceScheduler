15f89d00bc3db49b1d02ead6c87988eed06bce0e
Owen Campbell <owen.campbell@tanti.org.uk>
2017-05-15 10:23:44 +0100

    [#79] Add docstrings to resources.py

--- src/conference_scheduler/resources.py
+++ src/conference_scheduler/resources.py
@@ -3,6 +3,62 @@ from datetime import datetime
 
 
 class Slot(NamedTuple):
+    """A period of time at a venue in which an event can be scheduled
+
+    Parameters
+    ----------
+    venue : str
+        A human readable string
+    starts_at: datetime
+        The starting time for the time period
+    duration: int
+        The duration of the time period in minutes
+    capacity: int
+        This will be compared with :attr:`Event.demand` during computation of
+        the schedule to ensure that events are only scheduled in slots that can
+        accommodate them.
+    session: str
+        A human readable string which serves as a tag for similar time periods
+        e.g. 'morning', 'afternoon'
+
+    Example
+    -------
+    For a conference where:
+
+        * It will take place on 2016-09-17
+        * There are two rooms - 'Main Hall' and 'Small Room'
+        * The Main Hall can seat 500 people and the Small Room, 50
+        * It is intended to hold two 30 talks in the morning (from 09:30
+          to 10:00 and from 11:00 to 11:30) and two more in the afternoon
+          (from 14:00 to 14:30 and 15:00 to 15:30)
+
+    We would create the following eight objects::
+
+        Slot(
+            venue='Main Hall', starts_at=datetime(2016, 09, 17, 09, 30),
+            duration=30, capacity=500, session='morning')
+        Slot(
+            venue='Main Hall', starts_at=datetime(2016, 09, 17, 10, 00),
+            duration=30, capacity=500, session='morning')
+        Slot(
+            venue='Main Hall', starts_at=datetime(2016, 09, 17, 14, 00),
+            duration=30, capacity=500, session='afternoon')
+        Slot(
+            venue='Main Hall', starts_at=datetime(2016, 09, 17, 15, 00),
+            duration=30, capacity=500, session='afternoon')
+        Slot(
+            venue='Small Room', starts_at=datetime(2016, 09, 17, 09, 30),
+            duration=30, capacity=50, session='morning')
+        Slot(
+            venue='Small Room', starts_at=datetime(2016, 09, 17, 10, 00),
+            duration=30, capacity=50, session='morning')
+        Slot(
+            venue='Small Room', starts_at=datetime(2016, 09, 17, 14, 00),
+            duration=30, capacity=50, session='afternoon')
+        Slot(
+            venue='Small Room', starts_at=datetime(2016, 09, 17, 15, 00),
+            duration=30, capacity=50, session='afternoon')
+    """
     venue: str
     starts_at: datetime
     duration: int
@@ -11,6 +67,25 @@ class Slot(NamedTuple):
 
 
 class Event:
+    """An event (e.g. a talk or a workshop) that needs to be scheduled
+
+    Parameters
+    ---------
+    name : str
+        A human readable string
+    duration : int
+        The expected duration of the event in minutes
+    demand : int
+        The anticipated demand - e.g. the number of attendees expected
+        This will be compared with :attr:`Slot.capacity` during computation of
+        the schedule to ensure that events are only scheduled in slots that can
+        accommodate them.
+        Use 0 if the event could be scheduled in any slot.
+    tags : list or tuple, optional
+        of human readable strings
+    unavailability : list or tuple, optional
+        of :class:`resources.Slot` or :class:`resources.Event`
+    """
 
     def __init__(self, name, duration, demand, tags=None, unavailability=None):
         self.name = name
@@ -78,6 +153,13 @@ class Event:
 
 
 class ScheduledItem(NamedTuple):
+    """Represents that an event has been scheduled to occur in a slot
+
+    Paramters
+    ---------
+    event : :class:`resources.Event`
+    slot : :class:`resources.Slot`
+    """
     event: Event
     slot: Slot
 
