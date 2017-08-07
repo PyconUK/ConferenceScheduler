ae46607320d3d68714f37932e8dbd919ebb2b15c
Vince Knight <vincent.knight@gmail.com>
2017-08-07 14:58:07 +0100

    Implement neighbourhood swap.
    Take an event and moves it to a different slot, if the slot is currently
    occupied it swaps.

---
+++ tests/heuristics/test_heuristic_utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+from conference_scheduler.heuristics import utils as hu
+
+
+# Tests for array form
+
+def test_neighbourhood_move_to_unused():
+    array = np.array([
+        [0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0]
+    ])
+    np.random.seed(0)
+    X = hu.element_from_neighbourhood(array)
+    expected_array = np.array([
+        [0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0]
+    ])
+    assert np.array_equal(X, expected_array)
+
+def test_neighbourhood_swap_events():
+    array = np.array([
+        [0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0]
+    ])
+    np.random.seed(35)
+    X = hu.element_from_neighbourhood(array)
+    expected_array = np.array([
+        [0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0, 0]
+    ])
+    assert np.array_equal(X, expected_array)
