81922b01da708629c1f18a4f126428a72e1d56ca
Owen Campbell <owen.campbell@empiria.co.uk>
2017-08-08 13:33:32 +0100

    Bump version: 2.0.0 → 2.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='conference-scheduler',
-    version='2.0.0',
+    version='2.1.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/PyconUK/ConferenceScheduler',
